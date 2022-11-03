fee9605716f696d3a0b96e665b662d20857697ef
hanwu <ycyang2051@outlook.com>
2022-11-03 17:46:01 +0800

    项目初始化 路由+elementUI

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="nav">
+    <el-button type="primary">Primary</el-button>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style lang="less" scoped>
+
+</style>
