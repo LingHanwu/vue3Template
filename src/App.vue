b59aacc3e686db93de5f64bd4f9b379a383a45bb
hanwu <ycyang2051@outlook.com>
2022-11-03 18:00:07 +0800

    页面基本布局

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,20 @@
     <nav class="header">
       <span class="title">图书管理系统</span>
     </nav>
+
+    <!-- 侧边栏和主体 -->
+    <div class="container">
+      <!-- 侧边栏导航 -->
+      <div class="slider">
+        11
+      </div>
+      <!-- 主体数据 -->
+      <div class="main">
+
+      </div>
+    </div>
+
+
   </div>
 
 
@@ -23,6 +37,25 @@
   .title {
     margin: 25px;
     font-size: 24px;
+  }
+}
+
+.container {
+  display: flex;
+
+  // 侧边栏
+  .slider {
+    width: 200px;
+    height: calc(100vh - 60px);
+    overflow: hidden;
+    margin-right: 2px;
+    background-color: white;
+  }
+
+  // 主体数据
+  .main {
+    flex: 1;
+    background-color: aqua;
   }
 }
 </style>
