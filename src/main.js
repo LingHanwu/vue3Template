3a01f9186b39af9b320eb96f8f88c763c5c59ba0
hanwu <ycyang2051@outlook.com>
2022-11-03 17:54:20 +0800

    样式重置2

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'normalize.css'
+import './assets/css/index.css'
 createApp(App).use(router).mount('#app')
